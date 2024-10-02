b2cdafb0a2e1f84e8b71674ddb97cef53de6d04a
Gianna Solano <g_solano1@u.pacific.edu>
2024-10-02 15:22:24 -0700

    final merge push

--- GitTeamPrep/src/SimplePicture.java
+++ GitTeamPrep/src/SimplePicture.java
@@ -1,6 +1,6 @@
-import acm.graphics.GImage;
 //comment1
 //comment2
+import acm.graphics.GImage;
 import acm.graphics.GLabel;
 import acm.program.GraphicsProgram; 
 
@@ -13,13 +13,10 @@ public class SimplePicture extends GraphicsProgram {
 	}
 	
 	public void run() {
-		final GImage robot = new GImage("robot.png", 200, 100);
+		final GImage robot = new GImage(IMAGE_NAME, 200, 100);
 		add(robot);
-<<<<<<< HEAD
-		GLabel label = new GLabel("Step: 7 Test", 200, 400);
-=======
+
 		GLabel label = new GLabel("Hw I feel when using github hackslash/martyrr", 200, 400);
->>>>>>> branch 'main' of https://github.com/Fall2024Comp55/git-team-lab-team-c-c-g.git
 		add(label);
 	} 
 	
